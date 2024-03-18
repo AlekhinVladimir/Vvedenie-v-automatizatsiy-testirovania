7f00801935801ef902afd9582e2601facda9b022
AlekhinVladimir <119034591+AlekhinVladimir@users.noreply.github.com>
2024-03-18 10:35:24 +0400

    HW2

---
+++ HW2/src/main/java/Door.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * Класс для описания двери
+ */
+public class Door {
+
+    private boolean isPrize;
+
+    public Door(boolean isPrize) {
+        this.isPrize = isPrize;
+    }
+
+    public boolean isPrize() {
+        return isPrize;
+    }
+}
