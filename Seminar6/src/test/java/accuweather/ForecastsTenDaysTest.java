cefd2f60fe9b32c44ed6c8049914401b14d96d6e
AlekhinVladimir <119034591+AlekhinVladimir@users.noreply.github.com>
2024-04-01 10:29:11 +0400

    Seminar6

---
+++ Seminar6/src/test/java/accuweather/ForecastsTenDaysTest.java
@@ -0,0 +1,27 @@
+package accuweather;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import seminar.accuweather.weather.Weather;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThan;
+
+public class ForecastsTenDaysTest extends AccuweatherAbstractTest {
+
+    @Test
+    @Disabled
+
+    void testGetTenDayForecastResponse() {
+        // Изменено на 10day в запросе URL
+        Weather weather = given().queryParam("apikey", getApiKey()).pathParam("locationKey", 50)
+                .when().get(getBaseUrl() + "/forecasts/v1/daily/10day/{locationKey}")
+                .then().statusCode(200).time(lessThan(2000L))
+                .extract().response().body().as(Weather.class);
+        // Изменено условие ожидания на 10 дней
+        Assertions.assertEquals(10, weather.getDailyForecasts().size());
+        System.out.println(weather);
+    }
+
+}
